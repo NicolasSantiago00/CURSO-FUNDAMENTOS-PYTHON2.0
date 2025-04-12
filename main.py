7d9bf8fa98d9288d19b0e00624070dcdde1cf9ee
Nicolas <nbayona000@gmail.com>
2025-04-12 16:31:53 -0500

    mando ejercico de los modulos. Muchas gracias

---
+++ main.py
@@ -0,0 +1,12 @@
+# main.py
+
+import matematicas
+
+# Probamos las funciones con dos números
+a = 15
+b = 10
+
+print("Suma:", matematicas.suma(a, b))
+print("Resta:", matematicas.resta(a, b))
+print("Multiplicación:", matematicas.multiplicacion(a, b))
+print("División:", matematicas.division(a, b))
